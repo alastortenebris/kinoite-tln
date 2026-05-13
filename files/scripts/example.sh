ef18edb32b60ad97339ad333fe16a2271ce69859
Alastor Tenebris <100882069+alastortenebris@users.noreply.github.com>
2026-05-12 22:47:57 -0700

    Update example.sh

--- files/scripts/example.sh
+++ files/scripts/example.sh
@@ -11,3 +11,6 @@ echo 'Installing ProtonVPN'
 dnf install -y https://repo.protonvpn.com/fedora-44-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.4-1.noarch.rpm
 # The install errors out unless we do it like this.
 dnf install -y proton-vpn-gnome-desktop --setopt=install_weak_deps=False || true
+
+#fix ffmpeg
+dnf distro-sync
