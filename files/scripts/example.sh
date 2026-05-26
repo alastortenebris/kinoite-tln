7e4ecb71caac2f9081e307bc349d6c0a2bfd7f76
Alastor Tenebris <100882069+alastortenebris@users.noreply.github.com>
2026-05-25 21:08:13 -0700

    Update example.sh

--- files/scripts/example.sh
+++ files/scripts/example.sh
@@ -8,9 +8,9 @@ set -oue pipefail
 # Your code goes here.
 
 #echo 'Installing ProtonVPN'
-#dnf install -y https://repo.protonvpn.com/fedora-44-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.4-1.noarch.rpm
+dnf install -y https://repo.protonvpn.com/fedora-44-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.4-1.noarch.rpm
 # The install errors out unless we do it like this.
-#dnf install -y proton-vpn-gnome-desktop --setopt=install_weak_deps=False || true
+dnf install -y proton-vpn-gtk-app --setopt=install_weak_deps=False || true
 
 #fix ffmpeg
 echo 'Enabling Terra'
