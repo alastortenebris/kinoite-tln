3193a953d81395f42338fdef3000ac67b214f7f4
Alastor Tenebris <100882069+alastortenebris@users.noreply.github.com>
2026-05-30 17:20:09 -0700

    Update example.sh

--- files/scripts/example.sh
+++ files/scripts/example.sh
@@ -12,3 +12,9 @@ dnf install -y https://repo.protonvpn.com/fedora-44-stable/protonvpn-stable-rele
 # The install errors out unless we do it like this.
 dnf install -y proton-vpn-gtk-app --setopt=install_weak_deps=False || true
 
+echo 'Enabling RPMFusion'
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+echo 'Installing extras'
+dnf install -y libavcodec-freeworld --no-allow-downgrade
+dnf install -y mesa-va-drivers-freeworld.x86_64 --no-allow-downgrade
+dnf swap -y mesa-vulkan-drivers mesa-vulkan-drivers-freeworld
