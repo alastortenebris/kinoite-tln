e4e0cd03624da77c4f3b02bfa22cd5d9082812f7
Alastor Tenebris <100882069+alastortenebris@users.noreply.github.com>
2026-07-26 16:13:01 -0700

    Update example.sh

--- files/scripts/example.sh
+++ files/scripts/example.sh
@@ -8,9 +8,9 @@ set -oue pipefail
 # Your code goes here.
 
 #echo 'Installing ProtonVPN'
-dnf install -y https://repo.protonvpn.com/fedora-44-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.4-1.noarch.rpm
+#dnf install -y https://repo.protonvpn.com/fedora-44-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.4-1.noarch.rpm
 # The install errors out unless we do it like this.
-dnf install -y proton-vpn-gtk-app --setopt=install_weak_deps=False || true
+# dnf install -y proton-vpn-gtk-app --setopt=install_weak_deps=False || true
 
 echo 'Enabling RPMFusion'
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
